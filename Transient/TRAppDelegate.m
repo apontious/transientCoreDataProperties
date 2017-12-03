033a57c54d0f3e2b120aa35cba6b20032cc4a44f
Andrew Pontious <apontious@gmail.com>
2017-12-02 16:26:27 -0800

    Update copyright.

--- Transient/TRAppDelegate.m
+++ Transient/TRAppDelegate.m
@@ -3,7 +3,7 @@
 //  Transient Core Data Properties http://github.com/apontious/transientCoreDataProperties
 //
 //  Created by Andrew Pontious on 6/16/12.
-//  Copyright (c) 2012 Andrew Pontious.
+//  Copyright (c) 2012-2017 Andrew Pontious.
 //  Some right reserved: http://opensource.org/licenses/mit-license.php
 //
 
